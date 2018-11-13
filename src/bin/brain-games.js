e167d2d643432aedafab8d46cc8af702a025c145
brasid <alexanderfuf@gmail.com>
2018-11-13 10:36:15 +0300

    small logic update

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { greeting } from '..';
+import { greeting, yourName } from '..';
 
-console.log(greeting());
+const welcome = greeting() + yourName();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  const hello = console.log('Welcome to the Brain Games!');
+const greeting = () => console.log('Welcome to the Brain Games!');
+
+const yourName = () => {
   const gamerName = readlineSync.question('May I have your name? ');
   const hiName = console.log(`Hello, ${gamerName}!`);
-  return (hello, gamerName, hiName);
+  return (gamerName, hiName);
 };
 
-export { greeting };
+export { greeting, yourName };
