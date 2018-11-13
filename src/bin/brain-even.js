f0a9bc43705ca97361e912c9abd69d5877c11cf5
brasid <alexanderfuf@gmail.com>
2018-11-13 15:43:05 +0300

    add shebang

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,6 @@
-import {
-  greeting, brainEven,
-} from '..';
+#!/usr/bin/env node
+
+import { greeting, brainEven } from '..';
 
 console.log(greeting);
 console.log(brainEven());
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { greeting, hiName } from '..';
 
 console.log(greeting);
