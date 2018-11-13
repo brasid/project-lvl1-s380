7f5a8522e08148426021e278bfaf4ea5c8fa3adf
brasid <alexanderfuf@gmail.com>
2018-11-13 09:47:59 +0300

    fix misspelling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () =>  {
+const greeting = () => {
   const hello = console.log('Welcome to the Brain Games!');
   const gamerName = readlineSync.question('May I have your name? ');
   const hiName = console.log(`Hello, ${gamerName}!`);
   return (hello, gamerName, hiName);
 };
+
+export { greeting };
