f76335a2b98ad7f0c352c12009c786772eaf28d1
brasid <alexanderfuf@gmail.com>
2018-11-15 08:18:33 +0300

    move calc functions from math to game

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,13 +1,36 @@
 import { cons } from 'hexlet-pairs';
-import { getRandomNum, getRandomOperator, getResult } from '../math';
+import getRandomNum from '../math';
 import playGame from '..';
+
+const getRandomOperator = () => {
+  const num = Math.floor(Math.random() * 3); // expect output 0, 1 or 2
+  switch (num) {
+    case (0):
+      return '+';
+    case (1):
+      return '-';
+    default:
+      return '*';
+  }
+};
+
+const doCalculation = (num1, num2, operator) => {
+  switch (operator) {
+    case ('+'):
+      return num1 + num2;
+    case ('-'):
+      return num1 - num2;
+    default:
+      return num1 * num2;
+  }
+};
 
 const taskAndAnswer = () => {
   const firstNum = getRandomNum();
   const secondNum = getRandomNum();
   const randomOperator = getRandomOperator();
-  const gameTask = `Question: ${firstNum} ${randomOperator} ${secondNum}  `;
-  const correctAnswer = getResult(firstNum, secondNum, randomOperator);
+  const gameTask = `${firstNum} ${randomOperator} ${secondNum}`;
+  const correctAnswer = doCalculation(firstNum, secondNum, randomOperator);
   return cons(gameTask, correctAnswer);
 };
 
