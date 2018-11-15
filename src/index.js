22f2233992992144e3faacd34735cb5a07f10849
brasid <alexanderfuf@gmail.com>
2018-11-15 13:07:11 +0300

    change index structure

--- src/index.js
+++ src/index.js
@@ -2,51 +2,38 @@ import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
 const playGame = (taskAndAnswer, gameRule) => {
-  const doGreeting = () => {
-    console.log('Welcome to the Brain Games!\n');
-    console.log(`${gameRule}\n`);
-    const playerName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${playerName}!\n`);
-    return (playerName);
-  };
 
-  const getRoundResult = (playerName) => {
-    let isRoundPassed = true;
+    console.log(`Welcome to the Brain Games!\n${gameRule}`);
+    const playerName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${playerName}!`);
+    const firstRound = 1;
+    const lastRound = 3;
+    if (gameProcess(firstRound, lastRound, playerName)) {
+      console.log(`Congratulations, ${playerName}!\n`);
+    } else {
+      console.log('I\'m sorry, little one.');
+    }
+    return (playerName);
+};
+
+const gameProcess = (round, lastRound, playerName) => {
+  if (round > lastRound) {
+      return true;
+    }
     const getTaskAndAnswer = taskAndAnswer();
     const gameTask = `Question ${car(getTaskAndAnswer)}  `;
     const correctAnswer = cdr(getTaskAndAnswer);
     const playerAnswer = readlineSync.question(gameTask);
     if (playerAnswer !== String(correctAnswer)) {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}\n`);
-      isRoundPassed = false;
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}`);
     } else {
-      console.log(`Your answer: ${playerAnswer}`);
-      console.log('Correct!\n');
+      console.log(`Your answer: ${playerAnswer}\nCorrect!`);
+      return gameProcess(round + 1, lastRound, playerName);
     }
-    return isRoundPassed;
+
   };
 
-  const isPlayerWin = (round, lastRound, playerName) => {
-    if (round > lastRound) {
-      return true;
-    }
-    return (getRoundResult(playerName)) ? isPlayerWin(round + 1, lastRound, playerName) : false;
-  };
 
-  const firstRound = 1;
-  const lastRound = 3;
-
-  const gameProcess = (playerName) => {
-    if (isPlayerWin(firstRound, lastRound, playerName)) {
-      console.log(`Congratulations, ${playerName}!\n`);
-    } else {
-      console.log('I\'m sorry, little one.');
-    }
-  };
-
-  const playerName = doGreeting();
-  gameProcess(playerName);
 };
 
 export default playGame;
