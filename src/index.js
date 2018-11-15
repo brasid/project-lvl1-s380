3fb8f4502a631519bfd11f80c37d481ee2964640
brasid <alexanderfuf@gmail.com>
2018-11-15 13:33:34 +0300

    rewrite index to make two simple functions

--- src/index.js
+++ src/index.js
@@ -1,49 +1,34 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
+const isPlayerWin = (round, lastRound, playerName, taskAndAnswer) => {
+  if (round > lastRound) {
+    return true;
+  }
+  const getTaskAndAnswer = taskAndAnswer();
+  const gameTask = `Question ${car(getTaskAndAnswer)}  `;
+  const correctAnswer = cdr(getTaskAndAnswer);
+  const playerAnswer = readlineSync.question(gameTask);
+  if (playerAnswer !== String(correctAnswer)) {
+    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}`);
+    return false;
+  }
+  console.log(`Your answer: ${playerAnswer}\nCorrect!`);
+  return isPlayerWin(round + 1, lastRound, playerName, taskAndAnswer);
+};
+
 const playGame = (taskAndAnswer, gameRule) => {
-  const doGreeting = () => {
-    console.log(`Welcome to the Brain Games!\n${gameRule}`);
-    const playerName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${playerName}!`);
-    return (playerName);
-  };
-
-  const getRoundResult = (playerName) => {
-    let isRoundPassed = true;
-    const getTaskAndAnswer = taskAndAnswer();
-    const gameTask = `Question ${car(getTaskAndAnswer)}  `;
-    const correctAnswer = cdr(getTaskAndAnswer);
-    const playerAnswer = readlineSync.question(gameTask);
-    if (playerAnswer !== String(correctAnswer)) {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}`);
-      isRoundPassed = false;
-    } else {
-      console.log(`Your answer: ${playerAnswer}\nCorrect!`);
-    }
-    return isRoundPassed;
-  };
-
-  const isPlayerWin = (round, lastRound, playerName) => {
-    if (round > lastRound) {
-      return true;
-    }
-    return (getRoundResult(playerName)) ? isPlayerWin(round + 1, lastRound, playerName) : false;
-  };
-
+  console.log(`Welcome to the Brain Games!\n${gameRule}`);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
   const firstRound = 1;
   const lastRound = 3;
-
-  const gameProcess = (playerName) => {
-    if (isPlayerWin(firstRound, lastRound, playerName)) {
-      console.log(`Congratulations, ${playerName}!\n`);
-    } else {
-      console.log('I\'m sorry, little one.');
-    }
-  };
-
-  const playerName = doGreeting();
-  gameProcess(playerName);
+  if (isPlayerWin(firstRound, lastRound, playerName, taskAndAnswer)) {
+    console.log(`Congratulations, ${playerName}!\n`);
+  } else {
+    console.log('I\'m sorry, little one.');
+  }
+  return (playerName);
 };
 
 export default playGame;
