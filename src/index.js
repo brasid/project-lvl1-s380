71f109df52d0cdf85ba00c4795085c25b2f6e49d
brasid <alexanderfuf@gmail.com>
2018-11-16 09:24:18 +0300

    move constants to global, remove unnecessary parameters in isPlayerWIn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
-const isPlayerWin = (round, lastRound, playerName, taskAndAnswer) => {
+const isPlayerWin = (round, lastRound, taskAndAnswer) => {
   if (round > lastRound) {
     return true;
   }
@@ -10,23 +10,25 @@ const isPlayerWin = (round, lastRound, playerName, taskAndAnswer) => {
   const correctAnswer = cdr(getTaskAndAnswer);
   const playerAnswer = readlineSync.question(gameTask);
   if (playerAnswer !== String(correctAnswer)) {
-    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}`);
+    console.log(`\n'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n`);
     return false;
   }
-  console.log(`Your answer: ${playerAnswer}\nCorrect!`);
-  return isPlayerWin(round + 1, lastRound, playerName, taskAndAnswer);
+  console.log(`\nYour answer: ${playerAnswer}\nCorrect!\n`);
+  return isPlayerWin(round + 1, lastRound, taskAndAnswer);
 };
 
+const firstRound = 1;
+const lastRound = 3;
+
 const playGame = (taskAndAnswer, gameRule) => {
-  console.log(`Welcome to the Brain Games!\n${gameRule}`);
+  console.log(`\nWelcome to the Brain Games!\n${gameRule}\n`);
   const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-  const firstRound = 1;
-  const lastRound = 3;
-  if (isPlayerWin(firstRound, lastRound, playerName, taskAndAnswer)) {
+  console.log(`Hello, ${playerName}!\n`);
+  if (isPlayerWin(firstRound, lastRound, taskAndAnswer)) {
     console.log(`Congratulations, ${playerName}!\n`);
   } else {
-    console.log('I\'m sorry, little one.');
+    console.log(`Let's try again, ${playerName}`);
+    console.log('I\'m sorry, little one.\n');
   }
 };
 
